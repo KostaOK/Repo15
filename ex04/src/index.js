e97560eb65fc2dd975ec7df02fb0f24603188575
Niko Kostič <kostic.niko@icloud.com>
2022-04-11 21:43:33 +0200

    Array Mutation

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+const milili = [10, 25, 4];
+
+function myArr(mili) {
+    "use strict";
+    milili[0] = 4;
+    milili[1] = 10;
+    milili[2] = 25;
+    console.log(mili)
+    return mili;
+}
+
+// Only change code above this line
+module.exports = myArr(milili);
